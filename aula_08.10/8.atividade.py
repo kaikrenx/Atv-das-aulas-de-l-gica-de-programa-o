6a8426fd64da7000c97f9a4aab8b7de8bfca9f8c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-09 14:50:39 -0300

    feat: fim da aula

--- aula_08.10/8.atividade.py
+++ aula_08.10/8.atividade.py
@@ -3,9 +3,16 @@ os.system("cls")
 
 
 def nota_final(primeira_nota, segunda_nota):
-    return primeira_nota + segunda_nota / 2 
-media = nota_final(primeira_nota, segunda_nota)
-
+    return (primeira_nota + segunda_nota) / 2 
 
 primeira_nota = float(input("Digite uma nota: "))
 segunda_nota = float(input("Digite uma nota: "))
+
+media = nota_final(primeira_nota, segunda_nota)
+
+
+
+if media>= 7:
+    print(f"Você foi aprovado, aqui sua média: {media}")
+else:
+    print(F"Você foi Reprovado: {media}")
