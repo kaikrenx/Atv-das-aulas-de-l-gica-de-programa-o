64bb088e34003124202fdcccb33e38340bfdc12d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-22 14:04:53 -0300

    feat: fim da aula

--- aula_08.10/8.atividade.py
+++ aula_08.10/8.atividade.py
@@ -3,12 +3,22 @@ os.system("cls")
 
 
 def nota_final(primeira_nota, segunda_nota):
+    os.system("cls")
     return (primeira_nota + segunda_nota) / 2 
 
-primeira_nota = float(input("Digite uma nota: "))
-segunda_nota = float(input("Digite uma nota: "))
 
-media = nota_final(primeira_nota, segunda_nota)
+while True: 
+
+    primeira_nota = float(input("Digite uma nota: "))
+    segunda_nota = float(input("Digite uma nota: "))
+
+    if primeira_nota and segunda_nota >= 0 and primeira_nota and segunda_nota  <= 10:
+        media = nota_final(primeira_nota, segunda_nota)
+        break
+    else: 
+        print("")
+        print("Tente novamente (apenas notas de 0 a 10)")
+        print("")
 
 
 
